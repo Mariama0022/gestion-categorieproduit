8f7a843e2881e2ac07367b1c8f449bc9e376de40
bamariama952-sketch <bamariama952@gmail.com>
2026-07-01 00:24:05 +0000

    initialisation tableau categories

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+// 1 
+
+$categories = [
+
+   0 =>      [
+            "code" => "1234",
+            "nom" => "categorie0",
+            "produits" => [
+                  0 => [
+                    "nom" => "produit1",
+                    "reference" => "reference1",
+                    "prix" => 1500,
+                    "quantite" => 5 
+                  ],
+                  1 => [
+                    "nom" => "produit2",
+                    "reference" => "reference2",
+                    "prix" => 2500,
+                    "quantite" => 3 
+                  ]
+            ]
+         ],
+   1 =>      [
+            "code" => "2222",
+            "nom" => "categorie1",
+            "produits" => []
+         ]
+];
