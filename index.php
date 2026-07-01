ab84a9bf01706b97636dbd45c094820c5a0fd7c0
bamariama952-sketch <bamariama952@gmail.com>
2026-07-01 00:48:01 +0000

    afficheCategorieSansProduit

--- index.php
+++ index.php
@@ -28,3 +28,16 @@ $categories = [
             "produits" => []
          ]
 ];
+
+
+
+// 2
+
+ function afficheCategorieSansProduit(array $categories): void{
+    foreach ($categories as  $categorie ) {
+        if (empty($categorie["produits"])) {
+            echo $categorie["nom"]."\n";
+        }
+    }
+ }
+ afficheCategorieSansProduit($categories);
